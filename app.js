6c0b81f32f9e12094818b4f6e13f37a3bb53e730
Aladdin Sonni <alaaden3000@gmail.com>
2016-03-23 20:27:42 +0200

    #1

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
