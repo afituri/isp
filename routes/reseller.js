dc381d76ca9f97305a7021788d3da23d00d6fb7d
abdooageel <ageel.abdoo@gmail.com>
2016-04-01 19:12:01 +0200

    re

--- routes/reseller.js
+++ routes/reseller.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 /* Add new reseller  */
 router.post('/add', function(req, res) {
   // console.log(req.body);
+  console.log("im hear");
   resellerMgr.addReseller(req.body,function(reseller){
     res.send(reseller);
   });
