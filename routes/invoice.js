1751ecf2f5049c27e34094f9937282fbc20e76f7
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2016-05-03 21:32:59 +0200

    all

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var model = require('../models');
-var userHelpers = require("../controller/userHelpers");
 var invoiceMgr = require("../controller/invoice");
 var invoicePMgr = require("../controller/invoice");
+
 
 
 /* GET all invoice */
 router.get('/:limit/:page', function(req, res) {
   invoiceMgr.getInvoice(req.params.limit,req.params.page,function(invoices){
     res.send(invoices);
-});
+	});
+ });
 
 router.get('/all', function(req, res) {
   invoiceMgr.getAllInvoices(function(policies){
@@ -29,7 +29,6 @@ router.put('/edit/:id', function(req, res) {
   invoiceMgr.updateInvoice(req.params.id,req.body,function(result){
     res.send(result);
   });
-
 });
 
 /* Delete invoice  by id  */
@@ -38,4 +37,6 @@ router.delete('/delete/:id', function(req, res) {
     res.send({result:result});  
   });
 });
+
+module.exports = router;
 
