33b95b96f21493e2a8d1ae76c97d5e8a3e56b06b
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2016-05-04 20:46:49 +0200

    All

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   'user':'',
   'password': '',
   'url': 'mongodb://root:102030@aws-us-east-1-portal.14.dblayer.com:11156/isp'
+  //'url': '127.0.0.1:27017/isp'
   // 'epassword' : '2015naga2016',
   // 'host': 'http://localhost:3000'
 }; 
