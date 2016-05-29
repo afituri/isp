3971338d9d65b2e427b8088d8875f288bb5c259a
Aladdin Sonni <alaaden3000@gmail.com>
2016-05-29 12:14:18 +0200

    #29

---
+++ public/js/resellerApp.js
@@ -0,0 +1,33 @@
+(function(){
+  'use strict';
+  var app = angular.module('reseller',[
+    'mgcrea.ngStrap',
+    'ngAnimate',
+    'ngSanitize',
+    'ui.router',
+    'jcs-autoValidate',
+    'toastr',
+    'ui.bootstrap',
+    'nya.bootstrap.select',
+    'oc.lazyLoad'
+  ]);
+  app.config(['$stateProvider','$urlRouterProvider','$locationProvider','$popoverProvider','$modalProvider','toastrConfig','$datepickerProvider','$ocLazyLoadProvider',function($stateProvider,$urlRouterProvider,$locationProvider,$popoverProvider,$modalProvider,toastrConfig,$datepickerProvider,$ocLazyLoadProvider){
+    $stateProvider.state('dashboard',{
+      url: '/',
+      templateUrl: 'pages/reseller/all/dashboard.html',
+      controller: 'DashboardCtl',
+      resolve: {
+        deps: ['$ocLazyLoad', function($ocLazyLoad) {
+          return $ocLazyLoad.load([{
+            insertBefore: '#ng_load_reseller_controler_before',
+            files: [
+              '/js/resellerControllers.js',
+            ] 
+          }]);
+        }] 
+      }
+    });
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(false).hashPrefix('!');
+  }]);
+}());
---
+++ public/js/resellerControllers.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+  var app = angular.module('reseller');
+  app.controller('DashboardCtl',['$scope',function($scope){
+    
+  }]);
+}());
