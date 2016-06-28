bc3e6fdfd4ad9b8aa9e2d954ecd83658405ac7e4
Aladdin Sonni <alaaden3000@gmail.com>
2016-06-28 15:47:47 +0200

    Add CSV Buttom

---
+++ public/js/controllers/restoreCtl.js
@@ -0,0 +1,27 @@
+(function(){
+  'use strict';
+  var app = angular.module('isp');
+  app.controller('RestoreCtl',['$scope','CSVServ','toastr',function($scope,CSVServ,toastr){
+    $scope.csv = {
+      result: null,
+      encoding: 'UTF-8',
+    };
+    $scope.CSVResult = false;
+    $scope.addCSV = function(){
+      if($scope.csv.result == null){
+        $scope.CSVResult = true;
+      } else {
+        CSVServ.addCSVFile($scope.csv.result).then(function(response) {
+          if(response.data){
+            toastr.success('تمت استعادة ملف CSV بنجاح');
+          } else {
+            console.log(response.data);
+          }
+        }, function(response) {
+          console.log("Something went wrong");
+        });
+        console.log($scope.csv);
+      }
+    };
+  }]);
+}());
