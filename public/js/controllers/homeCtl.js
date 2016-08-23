fd7b5ebcaf3c63fb12246e96ae739355853a8112
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2016-08-23 21:24:35 +0200

    #1

--- public/js/controllers/homeCtl.js
+++ public/js/controllers/homeCtl.js
@@ -5,18 +5,82 @@
     $scope.Pages ={}
     $scope.dollarPage = "sss";
      PermissionServ.getSubpermission().then(function(response){
-      //dollar
-      $scope.dollarPage = response.data[0].all;
-      //resselers
-      $scope.resselersPage = response.data[1].all;
-      console.log(response.data[2]);
-      
+        
+        $scope.permission =true;
+        if(response.data[0] != undefined){
+          //employee
+          $scope.permission =false;
+        //dollar
+        $scope.dollarPage = response.data[0].all;
+        //resselers
+        $scope.resselersPage = response.data[1].all;
+        //invoice
+        $scope.invoicePage = response.data[2].all;
+        //service
+        $scope.servicePage = response.data[3].all;
+        //suppliers
+        $scope.suppliersPage = response.data[4].all;
+        //serviceProviders
+        $scope.serviceProvidersPage = response.data[5].all;
+        //warehouses
+        $scope.warehousesPage = response.data[6].all;
+        //instock
+        $scope.instockPage = response.data[7].all;
+        //productServices
+        $scope.productServicesPage = response.data[8].all;
+        //productItems
+        $scope.productItemsPage = response.data[9].all;
+        //productPackages
+        $scope.productPackagesPage = response.data[10].all;
+
+        if(($scope.productPackagesPage||$scope.productItemsPage||$scope.productServicesPage)==false){
+          $scope.allProductService =false;
+        } else {
+           $scope.allProductService =true;
+        }
+        $scope.customersPage = response.data[11].all;
+        //productPolicies
+        $scope.productPoliciesPage = response.data[12].all;
+         //policies
+         $scope.policiesPage = response.data[13].all;      
+      } else {
+
+        //admin
+
+        $scope.dollarPage = true;
+        //resselers
+        $scope.resselersPage = true;
+        //invoice
+        $scope.invoicePage = true;
+        //service
+        $scope.servicePage = true;
+        //suppliers
+        $scope.suppliersPage = true;
+        //serviceProviders
+        $scope.serviceProvidersPage = true;
+        //warehouses
+        $scope.warehousesPage = true;
+        //instock
+        $scope.instockPage = true;
+        //productServices
+        $scope.productServicesPage = true;
+        //productItems
+        $scope.productItemsPage = true;
+        //productPackages
+        $scope.productPackagesPage = true;
+        $scope.allProductService =true;
+       
+        $scope.customersPage = true;
+        //productPolicies
+        $scope.productPoliciesPage =true;
+         //policies
+         $scope.policiesPage = true;      
+
+      }
 
 
 
-      console.log(response.data[3]);
-      console.log(response.data[4]);
-      console.log(response.data[5]);
+
 
 
     },function(response){
