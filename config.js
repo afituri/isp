c666440827044349726499c3c7302b6a5a8bb763
Ahmed Fituri <ahmed.elfituri@gmail.com>
2017-02-16 21:54:41 -0500

    Update config.js
    adding the ability to add admin creds

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ module.exports = {
 	'db': 'isp',
 	'user':'',
 	'password': '',
-	'url': '127.0.0.1:27017/isp'
+	'url': '127.0.0.1:27017/isp',
+	'admin_user' : '',
+	'admin_pass' : '',
   // 'epassword' : '2015naga2016',
   // 'host': 'http://localhost:3000'
 };
